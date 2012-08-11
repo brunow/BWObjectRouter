e2fc8f231e081c4b7fef1b1312e4679a439e8f7b
Bruno Wernimont <bruno.wernimont@gmail.com>
2012-08-11 09:11:59 +0200

    Change copyright project name

--- BWObjectRouter/BWObjectRouter.m
+++ BWObjectRouter/BWObjectRouter.m
@@ -1,6 +1,6 @@
 //
 // Created by Bruno Wernimont on 2012
-// Copyright 2012 BWLongTextViewController
+// Copyright 2012 BWObjectRouter
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
